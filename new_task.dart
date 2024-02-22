3dcaee51ad76a72baa5a4275f82313683726f2d6
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-22 22:20:28 +0400

    second commit

---
+++ new_task.dart
@@ -0,0 +1,15 @@
+// Daxili for loopundan istifadə edərək aşağıdakı nümunəni qurmaq üçün Dart proqramını yazın.
+// Netice :
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+// Beş ədəddən ən böyüyü tapmaq üçün Dart şərti ifadəsini yazın. Nəticələri göstərmək üçün xəbərdarlıq qutusunu göstərin.
+// List ve if şərti ilə yaza bilərsiniz;
+// Nümunə nömrələri: -5, -2, -6, 0, -1
+// Netice : 0
+
+
